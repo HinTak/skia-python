8e5734355502b3bcb800eee43cf4acf29fa19d11
Hin-Tak Leung <htl10@users.sourceforge.net>
2025-08-11 19:26:57 +0100

    Use x64_64 gn binary for arm64 windows

--- scripts/build_Windows.sh
+++ scripts/build_Windows.sh
@@ -15,6 +15,7 @@ cd skia && \
     patch -p1 < ../patch/skia-m140-minimize-download.patch && \
     patch -p1 < ../patch/skia-m132-colrv1-freetype.diff && \
     patch -p1 -R < ../patch/0001-Disable-OpenGL-for-Windows-on-ARM64.patch && \
+    patch -p1 < ../patch/fetch-gn-windows-arm64.diff && \
     python tools/git-sync-deps && \
     bin/gn gen out/Release --args="
 is_official_build=true
