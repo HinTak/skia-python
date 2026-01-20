c4d2635e7ef9a9fd85d79abcf5421a15b1907f2b
Hin-Tak Leung <htl10@users.sourceforge.net>
2026-01-20 01:23:19 +0000

    Missing && in the patch commands

--- scripts/build_macOS.sh
+++ scripts/build_macOS.sh
@@ -22,11 +22,11 @@ function apply_patch {
 }
 
 cd skia && \
-    patch -R -p1 < ../patch/0001-Proper-copy-and-move-for-path-classes.patch       \
-    patch -R -p1 < ../patch/0001-Conditionally-include-SkGradientShader.h.patch    \
-    patch -R -p1 < ../patch/0001-Remove-unused-methods.patch                       \
-    patch -R -p1 < ../patch/0001-Remove-obsolete-types-in-SkPath.h.patch           \
-    patch -R -p1 < ../patch/0001-Remove-dead-code-related-to-SkPathRef.patch       \
+    patch -R -p1 < ../patch/0001-Proper-copy-and-move-for-path-classes.patch    && \
+    patch -R -p1 < ../patch/0001-Conditionally-include-SkGradientShader.h.patch && \
+    patch -R -p1 < ../patch/0001-Remove-unused-methods.patch                    && \
+    patch -R -p1 < ../patch/0001-Remove-obsolete-types-in-SkPath.h.patch        && \
+    patch -R -p1 < ../patch/0001-Remove-dead-code-related-to-SkPathRef.patch    && \
     patch -R -p1 < ../patch/0001-Reland-Make-SkPath-immutable-on-GN-build.patch && \
     patch -p1 < ../patch/skia-m145-minimize-download.patch && \
     patch -p1 < ../patch/skia-m132-colrv1-freetype.diff && \
